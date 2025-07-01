7d9d32fa4a8cb6b1c1a8358382ef3cd3d8bc2a6d
Liam Kyle Viray <i_liamkyle.viray@stratpoint.com>
2025-07-01 16:53:48 +0800

    Added interactivity to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,31 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import ThemeToggle from "./ThemeToggle";
 import { buttonVariants } from "./ui/button";
 
 export const Navbar = () => {
+  const [showNavbar, setShowNavbar] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const landingHeight = document.getElementById("home")?.offsetHeight || 0;
+      const scrollThreshold = landingHeight * 0.75;
+      const scrollPosition = window.scrollY;
+
+      setShowNavbar(scrollPosition >= scrollThreshold);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <header className="z-50 top-0 sticky bg-transparent flex items-center justify-center px-2 py-4">
-      <nav className="bg-[var(--secondaryBg)] border-1 rounded-xl w-[600px] py-3 items-center justify-center">
+    <header
+      className={`z-50 top-0 fixed left-1/2 transform -translate-x-1/2 bg-transparent flex items-center justify-center px-2 py-4 transition-opacity duration-300
+      ${showNavbar ? "opacity-100" : "opacity-0 pointer-events-none"}`}
+    >
+      <nav className="bg-[var(--secondaryBg)] shadow-xl rounded-xl w-[600px] py-3 items-center justify-center">
         <ul className="flex gap-3 items-center justify-center">
           <li
             className={`${buttonVariants({

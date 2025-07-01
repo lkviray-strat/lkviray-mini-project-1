06ca02ba427218575e0d7d6be1221047f214a736
Liam Kyle Viray <i_liamkyle.viray@stratpoint.com>
2025-07-01 18:24:18 +0800

    Addd font jetbrains mono

--- public/fonts/fonts.ts
+++ public/fonts/fonts.ts
@@ -3,29 +3,115 @@ import localFont from "next/font/local";
 export const quickSand = localFont({
   src: [
     {
-      path: "Quicksand-Light.ttf",
+      path: "Quicksand/Quicksand-Light.ttf",
       weight: "300",
       style: "normal",
     },
     {
-      path: "Quicksand-Regular.ttf",
+      path: "Quicksand/Quicksand-Regular.ttf",
       weight: "400",
       style: "normal",
     },
     {
-      path: "Quicksand-Medium.ttf",
+      path: "Quicksand/Quicksand-Medium.ttf",
       weight: "500",
       style: "normal",
     },
     {
-      path: "Quicksand-SemiBold.ttf",
+      path: "Quicksand/Quicksand-SemiBold.ttf",
       weight: "600",
       style: "normal",
     },
     {
-      path: "Quicksand-Bold.ttf",
+      path: "Quicksand/Quicksand-Bold.ttf",
       weight: "700",
       style: "normal",
+    },
+  ],
+});
+
+export const jetBrains = localFont({
+  src: [
+    {
+      path: "JetBrainsMono/JetBrainsMono-Thin.ttf",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "JetBrainsMono/JetBrainsMono-ExtraLight.ttf",
+      weight: "200",
+      style: "normal",
+    },
+    {
+      path: "JetBrainsMono/JetBrainsMono-Light.ttf",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "JetBrainsMono/JetBrainsMono-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "JetBrainsMono/JetBrainsMono-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "JetBrainsMono/JetBrainsMono-SemiBold.ttf",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "JetBrainsMono/JetBrainsMono-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "JetBrainsMono/JetBrainsMono-ExtraBold.ttf",
+      weight: "800",
+      style: "normal",
+    },
+    // Italic
+    {
+      path: "JetBrainsMono/JetBrainsMono-ThinItalic.ttf",
+      weight: "100",
+      style: "italic",
+    },
+    {
+      path: "JetBrainsMono/JetBrainsMono-ExtraLightItalic.ttf",
+      weight: "200",
+      style: "italic",
+    },
+    {
+      path: "JetBrainsMono/JetBrainsMono-LightItalic.ttf",
+      weight: "300",
+      style: "italic",
+    },
+    {
+      path: "JetBrainsMono/JetBrainsMono-Italic.ttf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "JetBrainsMono/JetBrainsMono-MediumItalic.ttf",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "JetBrainsMono/JetBrainsMono-SemiBoldItalic.ttf",
+      weight: "600",
+      style: "italic",
+    },
+    {
+      path: "JetBrainsMono/JetBrainsMono-BoldItalic.ttf",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "JetBrainsMono/JetBrainsMono-ExtraBoldItalic.ttf",
+      weight: "800",
+      style: "italic",
     },
   ],
 });

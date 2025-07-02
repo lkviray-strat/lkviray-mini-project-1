53151f6b2d66be88cc5643eae7f925329d1eaaf0
Liam Kyle Viray <i_liamkyle.viray@stratpoint.com>
2025-07-02 11:15:10 +0800

    Implemented form actions and validations

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,16 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatErrors(errors: any) {
+  const formattedErrors: Record<string, string> = {};
+  for (const key in errors) {
+    if (errors[key]) {
+      formattedErrors[key] = errors[key]?.join(". ") || "";
+    }
+  }
+  return formattedErrors;
 }

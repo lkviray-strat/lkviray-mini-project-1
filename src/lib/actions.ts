abe715148fb44f41b86092b3eafc628f9e014094
Liam Kyle Viray <i_liamkyle.viray@stratpoint.com>
2025-07-02 17:18:29 +0800

    Implemented resend to contact form

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -3,6 +3,11 @@
 import { z } from "zod";
 import { contactSchema } from "./validations";
 import { formatErrors } from "./utils";
+import { Resend } from "resend";
+import { EmailTemplate } from "@/components/EmailTemplate";
+import { EmailVerificationTemplate } from "@/components/EmailVerificationTemplate";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function actionContactForm(
   prevState: unknown,
@@ -16,6 +21,28 @@ export async function actionContactForm(
 
   try {
     await contactSchema.parseAsync(formValues);
+
+    await resend.batch.send([
+      {
+        from: `Portfolio <${process.env.RESEND_DOMAIN}>`,
+        to: `${process.env.RESEND_MY_EMAIL}`,
+        subject: `Portfolio Contact: Message from ${formValues.name}`,
+        react: EmailTemplate({
+          name: formValues.name,
+          email: formValues.email,
+          message: formValues.message,
+        }),
+      },
+      {
+        from: `Liam's Portfolio <${process.env.RESEND_DOMAIN}>`,
+        to: `${formValues.email}`,
+        subject: `Portfolio Contact: Thanks for reaching out!`,
+        react: EmailVerificationTemplate({
+          name: formValues.name,
+          message: formValues.message,
+        }),
+      },
+    ]);
 
     return {
       status: "SUCCESS",

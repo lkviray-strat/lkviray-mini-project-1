b2bd9c5ce01f112feb6b4648dee789eadac51c8e
Liam Kyle Viray <i_liamkyle.viray@stratpoint.com>
2025-07-02 09:34:44 +0800

    Created contact ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 import Navbar from "@/components/Navbar";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { quickSand } from "../../public/fonts/fonts";
+import ContactForm from "@/components/ContactForm";
 
 export default function Home() {
   return (
     <>
       <section
         id="home"
-        className="flex flex-col gap-2 min-h-screen items-center justify-center bg-[var(--primaryBg)]"
+        className="flex flex-col gap-2 min-h-screen items-center justify-center"
       >
         <div className="flex flex-col w-full px-9 -mt-20 text-center justify-center items-center">
           <h1
@@ -41,10 +42,46 @@ export default function Home() {
           </div>
         </div>
       </section>
-      <section
-        id="about"
-        className="flex flex-col gap-2 h-screen items-center justify-center"
-      ></section>
+      <div className="px-4 tablet:px-9 laptop:px-12">
+        <section
+          id="about"
+          className="flex flex-col items-center"
+        >
+          <div className="flex flex-col items-center px-9 w-full max-w-[950px] tablet:items-start">
+            <h1 className="mt-[6rem] laptop:mt-[8rem] font-bold text-[30px] lphone:text-[45px] tablet:text-[70px] tracking-tight">
+              About Me
+            </h1>
+
+            {/* PlaceHolder */}
+            <div className="h-[100vh]"></div>
+          </div>
+        </section>
+        <section
+          id="projects"
+          className="flex flex-col items-center"
+        >
+          <div className="flex flex-col items-center px-9 w-full max-w-[950px] tablet:items-start">
+            <h1 className="mt-[6rem] laptop:mt-[8rem] font-bold text-[30px] lphone:text-[45px] tablet:text-[70px] tracking-tight">
+              Projects
+            </h1>
+
+            {/* PlaceHolder */}
+            <div className="h-[100vh]"></div>
+          </div>
+        </section>
+        <section
+          id="contact"
+          className="flex flex-col items-center"
+        >
+          <div className="flex flex-col items-center px-9 w-full max-w-[950px] tablet:items-start">
+            <h1 className="mt-[6rem] laptop:mt-[8rem] font-bold text-[30px] lphone:text-[45px] tablet:text-[70px] tracking-tight">
+              Contact Me
+            </h1>
+
+            <ContactForm />
+          </div>
+        </section>
+      </div>
     </>
   );
 }

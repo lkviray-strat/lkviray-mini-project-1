1e6f7a255fd7e4f9b6357699198f4096d5ac247d
Liam Kyle Viray <i_liamkyle.viray@stratpoint.com>
2025-07-04 06:51:59 +0800

    Updated project page and card

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,5 +1,6 @@
 import { getProjectBySlug } from "@/lib/projects";
 import { getSkillsByName, Skill } from "@/lib/skills";
+import Image from "next/image";
 import React from "react";
 
 type ProjectPageProps = {
@@ -13,10 +14,34 @@ export async function ProjectPage({ params }: ProjectPageProps) {
   const project = getProjectBySlug(slug);
   const skills: Skill[] =
     (getSkillsByName(project?.skills ?? []) as Skill[]) || [];
+  const imageUrl = project?.imageUrl + "/" + project?.slug;
 
   return (
-    <div>
-      <h1>{project?.title}</h1>
+    <div className="px-4 tablet:px-9 laptop:px-12">
+      <section
+        id="projects"
+        className="flex flex-col items-center"
+      >
+        <div className="mt-[6rem] desktop:mt-[8rem] flex flex-col laptop:flex-row items-center px-9 w-full max-w-[1400px]">
+          <div className="bg-blue-600 shadow-lg w-full h-[650px] relative mb-2 rounded-2xl overflow-hidden">
+            <Image
+              alt={project?.title as string}
+              src={`${imageUrl}-1.png`}
+              fill
+              className="object-contain "
+            />
+          </div>
+
+          <div className="flex flex-col gap-3 w-[600px] laptop:ml-10">
+            <h1 className=" text-[20px] lphone:text-[35px] tablet:text-[30px] font-extrabold tracking-tight">
+              {project?.title}
+            </h1>
+            <p className="text-[13px] tablet:text-[16px] mb-4">
+              {project?.description}
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -4,6 +4,7 @@ import SkillBadge from "./SkillBadge";
 import Link from "next/link";
 import { getProjectBySlug } from "@/lib/projects";
 import { getSkillsByName, Skill } from "@/lib/skills";
+import Image from "next/image";
 
 type ProjectCardProps = {
   slug: string;
@@ -12,13 +13,22 @@ export const ProjectCard = async ({ slug }: ProjectCardProps) => {
   const project = getProjectBySlug(slug);
   const skills: Skill[] =
     (getSkillsByName(project?.skills ?? []) as Skill[]) || [];
+  const imageUrl = project?.imageUrl + "/" + project?.slug;
 
+  console.log(imageUrl);
   return (
     <Link
       href={`/projects/${project?.slug}`}
       className="flex flex-col p-4 p border-1 gap-4 rounded-2xl grow basis-[350px] max-w-[420px] shadow-lg hover:shadow-xl transition-shadow duration-300"
     >
-      <div className="w-full aspect-[3/3] bg-red-400 mb-2 rounded-2xl"></div>
+      <div className="w-full aspect-[3/3] relative mb-2 rounded-2xl overflow-hidden">
+        <Image
+          alt={project?.title as string}
+          src={`${imageUrl}-1.png`}
+          fill
+          className="object-cover"
+        />
+      </div>
       <h2 className="text-[18px] tablet:text-[21px] font-extrabold ">
         {project?.title}
       </h2>

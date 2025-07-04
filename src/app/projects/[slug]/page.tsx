b517f2e1013839a87f5e5e36fc91b4add54b9926
Liam Viray <i_liamkyle.viray@stratpoint.com>
2025-07-04 11:59:03 +0800

    Implemented multi image url and carousel

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,4 +1,5 @@
-import { getProjectBySlug } from "@/lib/projects";
+import ProjectCarousel from "@/components/ProjectCarousel";
+import { getProjectBySlug, Project } from "@/lib/projects";
 import { getSkillsByName, Skill } from "@/lib/skills";
 import Image from "next/image";
 import React from "react";
@@ -12,9 +13,7 @@ type ProjectPageProps = {
 export async function ProjectPage({ params }: ProjectPageProps) {
   const { slug } = await params;
   const project = getProjectBySlug(slug);
-  const skills: Skill[] =
-    (getSkillsByName(project?.skills ?? []) as Skill[]) || [];
-  const imageUrl = project?.imageUrl + "/" + project?.slug;
+  const imageUrl = project?.imageUrls?.[0];
 
   return (
     <div className="px-4 tablet:px-9 laptop:px-12">
@@ -23,16 +22,9 @@ export async function ProjectPage({ params }: ProjectPageProps) {
         className="flex flex-col items-center"
       >
         <div className="mt-[6rem] desktop:mt-[8rem] flex flex-col laptop:flex-row items-center px-9 w-full max-w-[1400px]">
-          <div className="bg-blue-600 shadow-lg w-full h-[650px] relative mb-2 rounded-2xl overflow-hidden">
-            <Image
-              alt={project?.title as string}
-              src={`${imageUrl}-1.png`}
-              fill
-              className="object-contain "
-            />
-          </div>
+          <ProjectCarousel project={project as Project} />
 
-          <div className="flex flex-col gap-3 w-[600px] laptop:ml-10">
+          <div className="flex flex-col gap-3 w-full max-w-[600px] laptop:ml-10">
             <h1 className=" text-[20px] lphone:text-[35px] tablet:text-[30px] font-extrabold tracking-tight">
               {project?.title}
             </h1>

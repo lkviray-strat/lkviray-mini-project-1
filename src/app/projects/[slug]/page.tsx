fca8aaf0d8c3133a9696b4932053ef16985bc884
Liam Viray <i_liamkyle.viray@stratpoint.com>
2025-07-04 16:01:21 +0800

    Enhanced ui on home and changed favicon

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,7 +1,10 @@
 import ProjectCarousel from "@/components/ProjectCarousel";
+import { Button } from "@/components/ui/button";
 import { getProjectBySlug, Project } from "@/lib/projects";
 import { getSkillsByName, Skill } from "@/lib/skills";
+import { Eye, Lock } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 type ProjectPageProps = {
@@ -25,12 +28,33 @@ export async function ProjectPage({ params }: ProjectPageProps) {
           <ProjectCarousel project={project as Project} />
 
           <div className="flex flex-col gap-3 w-full max-w-[600px] laptop:ml-10">
-            <h1 className=" text-[20px] lphone:text-[35px] tablet:text-[30px] font-extrabold tracking-tight">
+            <h1 className=" text-[20px] lphone:text-[35px] tablet:text-[30px] font-extrabold tracking-tight transition-colors duration-200 hover:text-blue-400">
               {project?.title}
             </h1>
             <p className="text-[13px] tablet:text-[16px] mb-4">
               {project?.description}
             </p>
+            <Button
+              asChild
+              className={`${project?.githubUrl?.length === 0 ? `w-fit bg-gray-300 text-black opacity-100 pointer-events-none select-none shadow-xl border-0` : `w-fit bg-blue-600 text-white hover:bg-blue-800 active:bg-blue-400 shadow-xl`}`}
+            >
+              {project?.githubUrl?.length === 0 ? (
+                <span className="flex items-center gap-3 !px-7">
+                  {" "}
+                  <Lock />
+                  GitHub is Private
+                </span>
+              ) : (
+                <Link
+                  href={project?.githubUrl ?? ""}
+                  target="_blank"
+                  className="flex items-center gap-3 !px-7"
+                >
+                  <Eye />
+                  View on GitHub
+                </Link>
+              )}
+            </Button>
           </div>
         </div>
       </section>

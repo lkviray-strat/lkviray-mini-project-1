067f91c0a1541ae864409a00a76981ce29e50ba6
Liam Kyle Viray <i_liamkyle.viray@stratpoint.com>
2025-07-07 02:22:32 +0800

    Added fadein to components

--- src/components/about/AboutSection.tsx
+++ src/components/about/AboutSection.tsx
@@ -1,51 +1,65 @@
 import { skills } from "@/lib/data/skills";
 import Image from "next/image";
 import SkillBadge from "../SkillBadge";
+import FadeInSection from "../motion/FadeInSection";
 
 const AboutSection = () => {
   return (
-    <div className="flex flex-col laptop:flex-row gap-5 items-center laptop:items-start justify-center w-full laptop:ml-2 mt-4 ">
-      <Image
-        src="/images/about-me-picture.jpg"
-        alt="Liam Kyle Viray"
-        width={380}
-        height={380}
-        className="rounded-full shadow-lg laptop:ml-10 order-1 laptop:order-2"
-      />
+    <div className="flex flex-col mb-10 laptop:flex-row gap-5 items-center laptop:items-start  justify-center w-full laptop:ml-2 mt-4 ">
+      <FadeInSection
+        delay={0.3}
+        className="order-1 laptop:order-2"
+      >
+        <Image
+          src="/images/about-me-picture.jpg"
+          alt="Liam Kyle Viray"
+          width={600}
+          height={600}
+          className="rounded-full shadow-lg laptop:ml-10 "
+        />
+      </FadeInSection>
 
       <div className="flex flex-col gap-3 w-full order-2 laptop:order-1">
-        <p className="col-span-2 mphone:col-span-3 text-[13px] desktop:text-[17px] mb-4">
-          I&apos;m <strong>Liam Kyle Viray</strong>, a passionate and aspiring
-          Full Stack Software Engineer with great interest on{" "}
-          <strong>React</strong> and <strong>Java</strong>. I graduated{" "}
-          <strong>Magna Cum Laude</strong> with a Bachelor of Science in
-          Computer Science from{" "}
-          <strong>Pamantasan ng Lungsod ng Maynila (PLM)</strong>. I have a
-          strong foundation in programming and a keen eye for detail. I enjoy
-          solving complex problems and exploring new technologies. I always take
-          my time to try on upskilling myself through various online
-          courses/tutorials and creating personal projects to enhance my skills.
-          <br />
-          <br />
-          Outside of coding, I enjoy playing video games, reading novels and
-          manhwas, watching F1, and spending time with my{" "}
-          <strong>girlfriend</strong>. Let me know if you want to connect or
-          collaborate on a project! I&apos;m always open to new opportunities
-          and challenges.
-        </p>
-        <h2 className="text-[19px] laptop:text-[35px] tracking-tight font-extrabold transition-colors duration-200 hover:text-blue-400">
-          Skills
-        </h2>
+        <FadeInSection delay={0.6}>
+          <p className="col-span-2 mt-5 laptop:mt-0 mphone:col-span-3 text-[13px] desktop:text-[17px] mb-4">
+            I&apos;m <strong>Liam Kyle Viray</strong>, a passionate and aspiring
+            Full Stack Software Engineer with great interest on{" "}
+            <strong>React</strong> and <strong>Java</strong>. I graduated{" "}
+            <strong>Magna Cum Laude</strong> with a Bachelor of Science in
+            Computer Science from{" "}
+            <strong>Pamantasan ng Lungsod ng Maynila (PLM)</strong>. I have a
+            strong foundation in programming and a keen eye for detail. I enjoy
+            solving complex problems and exploring new technologies. I always
+            take my time to try on upskilling myself through various online
+            courses/tutorials and creating personal projects to enhance my
+            skills.
+            <br />
+            <br />
+            Outside of coding, I enjoy playing video games, reading novels and
+            manhwas, watching F1, and spending time with my{" "}
+            <strong>girlfriend</strong>. Let me know if you want to connect or
+            collaborate on a project! I&apos;m always open to new opportunities
+            and challenges.
+          </p>
+        </FadeInSection>
 
-        <div className="flex flex-row flex-wrap gap-2 mb-4">
-          {skills.map((skill) => (
-            <SkillBadge
-              key={skill.name}
-              skillColor={skill.color}
-              skillName={skill.name}
-            />
-          ))}
-        </div>
+        <FadeInSection delay={0.9}>
+          <h2 className="text-[19px] laptop:text-[35px] w-fit tracking-tight font-extrabold transition-colors duration-200 hover:text-blue-400">
+            Skills
+          </h2>
+        </FadeInSection>
+
+        <FadeInSection delay={1.2}>
+          <div className="flex flex-row flex-wrap gap-2 mb-4">
+            {skills.map((skill) => (
+              <SkillBadge
+                key={skill.name}
+                skillColor={skill.color}
+                skillName={skill.name}
+              />
+            ))}
+          </div>
+        </FadeInSection>
       </div>
     </div>
   );
--- src/components/contact/ContactForm.tsx
+++ src/components/contact/ContactForm.tsx
@@ -6,6 +6,7 @@ import { toast } from "sonner";
 import ToastCustom from "../ToastCustom";
 import { Button } from "../ui/button";
 import FormErrorMessage from "./FormErrorMessage";
+import FadeInSection from "../motion/FadeInSection";
 
 export const ContactForm = () => {
   const [state, action, isPending] = useActionState(
@@ -33,26 +34,33 @@ export const ContactForm = () => {
       action={action}
       className="flex flex-col gap-4 w-full max-w-[600px] tablet:ml-2 mt-4"
     >
-      <p className="text-center text-[13px] tablet:text-[15px] desktop:text-[18px] tablet:text-left">
-        Let’s make something awesome together! Send me a message and I’ll get
-        right back to you as soon as possible.
-      </p>
+      <FadeInSection delay={0.6}>
+        <p className="text-center text-[13px] tablet:text-[15px] desktop:text-[18px] tablet:text-left">
+          Let’s make something awesome together! Send me a message and I’ll get
+          right back to you as soon as possible.
+        </p>
+      </FadeInSection>
 
       <div className="flex flex-col gap-1 mt-5">
-        <label
-          htmlFor="name"
-          className="font-semibold"
-        >
-          Name<span className="text-red-500 text-[12px] align-super">*</span>
-        </label>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          defaultValue={state?.fieldData?.name}
-          className="border-1 rounded-[12px] p-2 px-3.5 focus:outline-blue-600"
-          placeholder="Example Name"
-        />
+        <FadeInSection delay={0.8}>
+          <label
+            htmlFor="name"
+            className="font-semibold"
+          >
+            Name<span className="text-red-500 text-[12px] align-super">*</span>
+          </label>
+        </FadeInSection>
+        <FadeInSection delay={0.9}>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            defaultValue={state?.fieldData?.name}
+            className="border-1 rounded-[12px] p-2 px-3.5 focus:outline-blue-600 w-full"
+            placeholder="Example Name"
+          />
+        </FadeInSection>
+
         <FormErrorMessage
           error={state?.fieldErrors}
           errorFor="name"
@@ -61,20 +69,25 @@ export const ContactForm = () => {
       </div>
 
       <div className="flex flex-col gap-1">
-        <label
-          htmlFor="email"
-          className="font-semibold"
-        >
-          Email<span className="text-red-500 text-[12px] align-super">*</span>
-        </label>
-        <input
-          type="email"
-          id="email"
-          name="email"
-          defaultValue={state?.fieldData?.email}
-          className="border-1 rounded-[12px] p-2 px-3.5 focus:outline-blue-600"
-          placeholder="example.email@gmail.com"
-        />
+        <FadeInSection delay={1.1}>
+          <label
+            htmlFor="email"
+            className="font-semibold"
+          >
+            Email<span className="text-red-500 text-[12px] align-super">*</span>
+          </label>
+        </FadeInSection>
+        <FadeInSection delay={1.2}>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            defaultValue={state?.fieldData?.email}
+            className="border-1 rounded-[12px] p-2 px-3.5 focus:outline-blue-600 w-full"
+            placeholder="example.email@gmail.com"
+          />
+        </FadeInSection>
+
         <FormErrorMessage
           error={state?.fieldErrors}
           errorFor="email"
@@ -83,22 +96,27 @@ export const ContactForm = () => {
       </div>
 
       <div className="flex flex-col gap-1">
-        <label
-          htmlFor="message"
-          className="font-semibold"
-        >
-          Message
-          <span className="text-red-500 text-[12px] align-super">*</span>
-        </label>
-        <textarea
-          name="message"
-          id="message"
-          rows={5}
-          maxLength={500}
-          defaultValue={state?.fieldData?.message}
-          className="border-1 rounded-[12px] p-2 px-3.5 placeholder-gray-500 focus:outline-blue-600"
-          placeholder="Type your message here..."
-        ></textarea>
+        <FadeInSection delay={1.4}>
+          <label
+            htmlFor="message"
+            className="font-semibold"
+          >
+            Message
+            <span className="text-red-500 text-[12px] align-super">*</span>
+          </label>
+        </FadeInSection>
+        <FadeInSection delay={1.5}>
+          <textarea
+            name="message"
+            id="message"
+            rows={5}
+            maxLength={500}
+            defaultValue={state?.fieldData?.message}
+            className="border-1 rounded-[12px] p-2 px-3.5 placeholder-gray-500 focus:outline-blue-600 w-full"
+            placeholder="Type your message here..."
+          ></textarea>
+        </FadeInSection>
+
         <FormErrorMessage
           error={state?.fieldErrors}
           errorFor="message"
@@ -106,13 +124,15 @@ export const ContactForm = () => {
         />
       </div>
 
-      <Button
-        disabled={isPending}
-        type="submit"
-        className="my-2 mb-[180px] bg-blue-700 p-4 w-full tablet:w-fit !px-6 lphone:!py-4 laptop:!px-9 text-[15px] lphone:text-[15px] tablet:text-[18px] tablet:!py-5 text-white hover:bg-blue-800 active:bg-blue-400 shadow-xl"
-      >
-        {isPending ? "Sending..." : "Submit"}
-      </Button>
+      <FadeInSection delay={1}>
+        <Button
+          disabled={isPending}
+          type="submit"
+          className="my-2 mb-[180px] bg-blue-700 p-4 w-full tablet:w-fit !px-6 lphone:!py-4 laptop:!px-9 text-[15px] lphone:text-[15px] tablet:text-[18px] tablet:!py-5 text-white hover:bg-blue-800 active:bg-blue-400 shadow-xl"
+        >
+          {isPending ? "Sending..." : "Submit"}
+        </Button>
+      </FadeInSection>
     </form>
   );
 };

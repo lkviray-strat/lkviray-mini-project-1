107e2b0c0abacf595bddee07dd3131b5a8aa6851
Liam Kyle Viray <i_liamkyle.viray@stratpoint.com>
2025-07-07 02:23:04 +0800

    Added fadein to projects per card

--- src/components/project/ProjectSection.tsx
+++ src/components/project/ProjectSection.tsx
@@ -1,16 +1,10 @@
-import React from "react";
-import ProjectCard from "./ProjectCard";
 import { projects } from "@/lib/data/projects";
+import ProjectListOfCards from "./ProjectListOfCards";
 
 export const ProjectSection = () => {
   return (
     <div className="flex flex-wrap gap-7 mt-8 place-items-center justify-center px-8 max-w-[1400px]">
-      {projects.map((proj) => (
-        <ProjectCard
-          key={proj.slug}
-          slug={proj.slug}
-        />
-      ))}
+      <ProjectListOfCards projects={projects} />
     </div>
   );
 };
